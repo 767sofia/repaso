1283ca9e260c534c9bea11defc0d9e6b9d4e1f94
Felix Kevin Acarapi Rivero <kevin.acarapi@itscordoba.edu.ar>
2023-07-07 20:33:56 -0300

    guardo archivos

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const btn1 = document.querySelector("#button_html");
+const btn2 = document.querySelector("#button_css");
+const btn3 = document.querySelector("#button_js ");
+
+
+const openInfo = () => {
+    console.log("hola")
+    
+}
+
+btn1.addEventListener("click",openInfo)
+btn2.addEventListener("click",openInfo)
+btn3.addEventListener("click",openInfo)
